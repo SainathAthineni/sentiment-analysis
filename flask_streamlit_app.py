9085299e9efe334447438c82a68409bf2f3e7056
Group2loyalist <150629074+Group2loyalist@users.noreply.github.com>
2023-12-22 02:39:32 -0500

    Update flask_streamlit_app.py

--- flask_streamlit_app.py
+++ flask_streamlit_app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 from nltk.sentiment import SentimentIntensityAnalyzer
+from googletrans import Translator
 import nltk
+import emoji
 
 # Download resources for the NLTK library (only needs to be done once)
 nltk.download('vader_lexicon')
+
+# Function to translate English text to a target language
+def translate_text(text, target_language):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language)
+    return translation.text
 
 def analyze_sentiment(text):
     sia = SentimentIntensityAnalyzer()
@@ -20,8 +28,34 @@ def main():
     st.title("Sentiment Analysis Web App")
 
     user_input = st.text_area("Enter your text:")
+    
+    # Add checkboxes for translation and emoji analysis
+    translate_checkbox = st.checkbox("Translate to:")
+    target_language = st.selectbox("Select Language:", ["Telugu", "Hindi"])
+    emoji_checkbox = st.checkbox("Include Emoji Analysis")
+
     if st.button("Analyze"):
-        sentiment_result = analyze_sentiment(user_input)
+        if translate_checkbox:
+            # Translate English text to the selected language
+            if target_language == "Telugu":
+                user_input_translated = translate_text(user_input, 'te')
+            elif target_language == "Hindi":
+                user_input_translated = translate_text(user_input, 'hi')
+            else:
+                user_input_translated = user_input  # No translation
+            
+            st.write(f"{target_language} Translation: {user_input_translated}")
+        else:
+            user_input_translated = user_input
+
+        if emoji_checkbox:
+            # Analyze sentiment with emoji analysis
+            user_input_with_emojis = emoji.demojize(user_input_translated)
+            sentiment_result = analyze_sentiment(user_input_with_emojis)
+        else:
+            # Analyze sentiment without emoji analysis
+            sentiment_result = analyze_sentiment(user_input_translated)
+
         st.write(f"Sentiment: {sentiment_result}")
 
 if __name__ == "__main__":
